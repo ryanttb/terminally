1a6a4fb228a6e5e7b65db3321ea0aa67b5ba537e
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-10-10 22:13:25 -0400

    initial printing of text in 3x4 character set

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$( function( ) {
+  var outputCharWidth = 128;
+  var canvas = $( '<canvas width="4" height="5" />' );
+
+  var characters = $( '#characters' );
+  
+  $( '#convert' ).submit( function( ) {
+    var text = $( '#text' ).val();
+
+    var width = outputCharWidth;
+    var height = Math.ceil( text.length / outputCharWidth );
+
+    canvas[ 0 ].width = width * 4;
+    canvas[ 0 ].height = height * 5;
+
+    var context = canvas[0].getContext( '2d' );
+    context.imageSmoothingEnabled = false;
+    context.mozImageSmoothingEnabled = false;
+    context.msImageSmoothingEnabled = false;
+
+    context.clearRect( 0, 0, width, height );
+
+    var i = 0, code, row, col;
+
+    for ( ; i < text.length; i++ ) {
+      code = text.charCodeAt( i );
+
+      row = Math.floor( i / outputCharWidth );
+      col = i % outputCharWidth;
+
+      context.drawImage(characters[ 0 ], (code % 16) * 4, ( Math.floor( code / 16 ) - 2 ) * 5, 4, 5, col * 4, row * 5, 4, 5 );
+    }
+
+    $( '#output' ).prop( 'src', canvas[0].toDataURL( 'image/png' ) );
+    return false;
+  } );
+} );
